70f8809b0794be9fc6e09e6d468fc381a1c05ce1
Egor Zhukovsky <egor.zhukovskij.dev@gmail.com>
2022-01-21 20:44:11 +0300

    Task 1.5: Implement ProductDao and fix image display on webpage

--- src/main/java/com/es/phoneshop/model/product/Product.java
+++ src/main/java/com/es/phoneshop/model/product/Product.java
@@ -2,14 +2,19 @@ package com.es.phoneshop.model.product;
 
 import java.math.BigDecimal;
 import java.util.Currency;
+import java.util.Objects;
 
 public class Product {
     private Long id;
     private String code;
     private String description;
-    /** null means there is no price because the product is outdated or new */
+    /**
+     * null means there is no price because the product is outdated or new
+     */
     private BigDecimal price;
-    /** can be null if the price is null */
+    /**
+     * can be null if the price is null
+     */
     private Currency currency;
     private int stock;
     private String imageUrl;
@@ -19,6 +24,15 @@ public class Product {
 
     public Product(Long id, String code, String description, BigDecimal price, Currency currency, int stock, String imageUrl) {
         this.id = id;
+        this.code = code;
+        this.description = description;
+        this.price = price;
+        this.currency = currency;
+        this.stock = stock;
+        this.imageUrl = imageUrl;
+    }
+
+    public Product(String code, String description, BigDecimal price, Currency currency, int stock, String imageUrl) {
         this.code = code;
         this.description = description;
         this.price = price;
@@ -81,5 +95,36 @@ public class Product {
 
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return stock == product.stock && Objects.equals(id, product.id)
+                && Objects.equals(code, product.code)
+                && Objects.equals(description, product.description)
+                && Objects.equals(price, product.price)
+                && Objects.equals(currency, product.currency)
+                && Objects.equals(imageUrl, product.imageUrl);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, code, description, price, currency, stock, imageUrl);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", code='" + code + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", currency=" + currency +
+                ", stock=" + stock +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
     }
 }
--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,11 +3,15 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.NoSuchElementException;
 
-public class ArrayListProductDaoTest
-{
+import static org.junit.Assert.*;
+
+public class ArrayListProductDaoTest {
     private ProductDao productDao;
+    private Currency usd = Currency.getInstance("USD");
 
     @Before
     public void setup() {
@@ -15,7 +19,38 @@ public class ArrayListProductDaoTest
     }
 
     @Test
-    public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+    public void testFindProductsHaveResults() {
+        assertFalse(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void testGetProductNotNull(){
+        final long ID = 4l;
+        assertNotNull(productDao.getProduct(ID));
+    }
+
+    @Test
+    public void testGetProductsEquals() {
+        final long ID = 9l;
+        assertEquals("nokia3310", productDao.getProduct(ID).getCode());
+    }
+
+    @Test
+    public void testNoSaveProductsWithSameId() {
+        Product firstProduct = productDao.getProduct(12l);
+        Product secondProduct = new Product(12L, "otherPhone"
+                , "otherPhone"
+                , new BigDecimal(80)
+                , usd
+                , 30
+                , "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20C61.jpg");
+        productDao.save(secondProduct);
+        assertNotEquals(firstProduct, productDao.getProduct(12L));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testSuccessDeleteProduct() {
+        productDao.delete(4l);
+        assertNull(productDao.getProduct(4l));
     }
 }
